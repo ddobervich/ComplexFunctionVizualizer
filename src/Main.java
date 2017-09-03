b80c2d7963580ed77f4bf024512658b67b2dae7e
David Dobervich <david.dobervich@gmail.com>
2017-09-03 15:47:57 -0700

    add function to Square class and get it ready for display in main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 import processing.core.PApplet;
 
 public class Main extends PApplet {
-	public void setup() {
+	ComplexGraph graph;
 
+	public void setup() {
+		size(1200, 600);
+
+		graph = new Square(this);
+		graph.setComplexSourceRange(-2, 2, -2, 2);
+		graph.setComplexTargetRange(-4, 4, -4, 4);
+		graph.setSourceImage();
 	}
 
 	public void draw() {
--- src/Square.java
+++ src/Square.java
@@ -2,16 +2,18 @@ import org.apache.commons.math3.complex.Complex;
 
 import processing.core.PApplet;
 
-public class Square extends ComplexGraph{
+public class Square extends ComplexGraph {
 
-	public Square(double a, double b, double c, double d, PApplet window) {
-		super(a, b, c, d, window);
+	public Square(PApplet window) {
+		super(window);
 	}
 
 	@Override
+	/***
+	 * function to visualize: f(z) = z^2
+	 */
 	public Complex function(Complex in) {
-		// TODO Auto-generated method stub
-		return null;
+		return in.multiply(in);
 	}
 
 }
